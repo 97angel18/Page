bb50537e1dfaae995957608e86e1f1434a7bf106
97angel18 <97angelcondori18@gmail.com>
2023-12-05 23:44:14 -0400

    first commit

---
+++ src/views/Services.jsx
@@ -0,0 +1,23 @@
+// local imports
+import { services } from "../data";
+import FadeIn from "../components/FadeIn";
+
+const Services = () => {
+  return <div id='services' className="mt-[160px] max-w-[1490px] mx-auto px-10 flex flex-col xs:flex-row gap-12 xs:gap-6 xs:justify-between w-full">
+    {services.map((service, i) => (
+      <FadeIn key={i} delay={0.2} direction="down">
+        <img src={service.icon} className="max-h-[84px] max-w-[84px]" alt="servicesIcon" />
+        <div className="flex flex-col gap-1.5">
+          <h3 className="text-center lg:text-start text-2xl lg:text-[28px] text-fontBlack font-medium">
+            {service.title}
+          </h3>
+          <h6 className="text-center lg:text-start text-base lg:text-lg text-fontGray font-medium">
+            {service.subtitle}
+          </h6>
+        </div>
+      </FadeIn>
+    ))}
+  </div>;
+};
+
+export default Services;
